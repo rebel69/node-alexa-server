b8e6dc98250f660a198eb830f905ed0443d771cb
Michael Hollowell <storefixtures@yahoo.com>
2017-01-19 09:23:59 -0600

    Update index.js
    Added support for getting the heat target temperture

--- app_modules/alexa-radiothermostat/index.js
+++ app_modules/alexa-radiothermostat/index.js
@@ -16,8 +16,28 @@ thermostatApp.intent('getTemp', function(req, res) {
   request(process.env.THERMOSTAT_URL + '/tstat', function (error, response, body) {
     console.log('Error: ' + error, 'RESPONSE: ' + response, 'BODY: ' + body);
     body = JSON.parse(body);
-    res.say("Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
-    res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
+    switch(body.tmode){
+		case 0: // OFF
+			res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is currently turned off.");
+			res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is currently turned off.");
+			break;
+		case 1: // HEAT
+			res.say("Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_heat + " degrees.");
+			res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is set to heat and the target temperature is " + body.t_heat + " degrees.");
+			break;
+		case 2: // COOL
+			res.say("Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
+			res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is set to cool and the target temperature is " + body.t_cool + " degrees.");
+			break;
+		case 3: // AUTO
+			res.say("Thermostat current temperature is " + body.temp + " degrees, the thermostat is currently turned auto.");
+			res.card("Thermostat Skill","Thermostat current temperature is " + body.temp + " degrees, the thermostat is currently turned auto.");
+			break;
+		default:
+			res.say("Something went wrong please try again.");
+			res.card("Something went wrong please try again.");
+			break;
+	}
     res.send();
   });
   return false;
