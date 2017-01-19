46e9177136de759fbb7399362b44cf50aa42dbf3
Michael Hollowell <storefixtures@yahoo.com>
2017-01-19 15:24:45 -0600

    testing

--- app_modules/alexa-radiothermostat/index.js
+++ app_modules/alexa-radiothermostat/index.js
@@ -14,15 +14,32 @@ thermostatApp.intent('setTemp', function(req, res) {
 	switch(setToMode){
 		case "heat": case "hot": case "warm": // HEAT
 			request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_heat: parseFloat(setToTemp)}});
-			txtSetResponse = txtSetResponse + "the thermostat is set to heat and the temperture is set to " + parseInt(setToTemp) + " degrees";
+			txtSetResponse = txtSetResponse + "the thermostat is set to heat and the temperture is set to " + parseInt(setToTemp) + " degrees.";
 			break;
 		case "AC": case "cold": case "cool": case "air conditioner": // COOL
 			request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_cool: parseFloat(setToTemp)}});
-			txtSetResponse = txtSetResponse + "the thermostat is set to air conditioner and the temperture is set to " + parseInt(setToTemp) + " degrees";
+			txtSetResponse = txtSetResponse + "the thermostat is set to air conditioner and the temperture is set to " + parseInt(setToTemp) + " degrees.";
 			break;
 		case undefined: // The mode wasn't specified so I get the current mode and update just the temp and leave the mode the same
-
-			txtSetResponse = "landed on undefined";
+			switch(body.tmode){
+				case 0: // OFF
+					txtSetResponse = txtSetResponse + "the thermostat is currently turned off, please turn it on to change the temperture.";
+					break;
+				case 1: // HEAT
+					request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_heat: parseFloat(setToTemp)}});
+					txtSetResponse = txtSetResponse + "the thermostat is set to heat mode and the temperture is set to " + parseInt(setToTemp) + " degrees.";
+					break;
+				case 2: // COOL
+					request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_cool: parseFloat(setToTemp)}});
+					txtSetResponse = txtSetResponse + "the thermostat is set to air conditioner mode and the temperture is set to " + parseInt(setToTemp) + " degrees.";
+					break;
+				case 3: // AUTO
+					txtSetResponse = txtSetResponse + "this command can not be completed because the thermostat is currently set to auto mode.";
+					break;
+				default:
+					txtSetResponse = "Something went wrong please try again.";
+					break;
+			}
 			break;
 		default:
 			txtSetResponse = "Something went wrong please try again.";
@@ -46,13 +63,13 @@ thermostatApp.intent('getTemp', function(req, res) {
 			txtGetResponse = txtGetResponse + "the thermostat is currently turned off.";
 			break;
 		case 1: // HEAT
-			txtGetResponse = txtGetResponse + "the thermostat is set to heat and the target temperature is " + body.t_heat + " degrees.";
+			txtGetResponse = txtGetResponse + "the thermostat is set to heat mode and the target temperature is " + body.t_heat + " degrees.";
 			break;
 		case 2: // COOL
-			txtGetResponse = txtGetResponse + "the thermostat is set to cool and the target temperature is " + body.t_cool + " degrees.";
+			txtGetResponse = txtGetResponse + "the thermostat is set to cool mode and the target temperature is " + body.t_cool + " degrees.";
 			break;
 		case 3: // AUTO
-			txtGetResponse = txtGetResponse + "the thermostat is currently turned auto.";
+			txtGetResponse = txtGetResponse + "the thermostat is currently set to auto mode.";
 			break;
 		default:
 			txtGetResponse = "Something went wrong please try again.";
@@ -68,7 +85,7 @@ thermostatApp.intent('getTemp', function(req, res) {
 // process launch request when no utterances detected
 thermostatApp.launch(function(req, res) {
   console.log('REQUEST', JSON.stringify(req));
-  res.say("You can say, what is the temperature, or set the temperature to 75!");
+  res.say("You can say, what is the temperature, or set heat to 72, or set the temperature to 75!");
 });
 
 module.exports = function(cb) {
